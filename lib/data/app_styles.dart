101e673e42c294683ae66c28bad74ab81ebb2b74
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-07-08 15:56:33 +0300

    Add files structure

---
+++ lib/data/app_styles.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xFFED0148);
+  static const text = Color.fromRGBO(91, 91, 91, 1);
+  static const disabled = Color.fromARGB(255, 202, 202, 202);
+  static const background = Colors.white;
+}
